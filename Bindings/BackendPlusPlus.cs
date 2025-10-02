d0b839a1340bf769f8ba3cf44789d2fa27648ba4
TIGames <kisstp2006@gmail.com>
2025-10-02 10:01:35 +0200

    Add project files.

---
+++ Bindings/BackendPlusPlus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Frontend.Bindings
+{
+    internal class BackendPlusPlus
+    {
+        [DllImport("Backend.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int AddNumbers(int a, int b);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("Backend.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int AddNumbers(int a, int b);
+
+    static void Main(string[] args)
+    {
+        int result = AddNumbers(5, 7);
+        Console.WriteLine($"Result from C++: {result}");
+    }
+}
